f049e889734c275a4212d24ced3d2c284dacc25b
Joseph Camyre <jwcamry03@gmail.com>
2021-12-23 16:45:06 -0800

    Implement Carousel with Person component, however the automatic interval not working

--- displaysite/src/components/People/Testing.jsx
+++ displaysite/src/components/People/Testing.jsx
@@ -14,49 +14,35 @@ function Testing() {
     console.log(JSON.stringify(Students));
     console.log(Students[0], Students[1], Students[2], Students[3]);
 
-    setInterval(function(){
-        setStudent1(Students[i%4]);
-        setStudent2(Students[(i+1)%4]);
-        // console.log(i, i%4);
-        console.log('********************');
-        i+=2;
-    }, 30000);
 
     return (
         <div>
             <h1>Testing</h1>
             <h2>Students</h2>
-            <Carousel interval={4000} controls={false} indicators={false}>
-        <Carousel.Item>
-          <img
-            src={
-              "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fthedelightfullaugh.com%2Fwp-content%2Fuploads%2F2020%2F09%2Fsmashed-double-cheeseburger.jpg&f=1&nofb=1"
-            }
-            alt=""
-          />
-          <img
-            src={
-              "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fthedelightfullaugh.com%2Fwp-content%2Fuploads%2F2020%2F09%2Fsmashed-double-cheeseburger.jpg&f=1&nofb=1"
-            }
-            alt=""
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            src={
-              "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.dubbysburgers.com%2Fwp-content%2Fuploads%2F2020%2F07%2FPBP_4641.jpg&f=1&nofb=1"
-            }
-            alt=""
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            src={
-              "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.gannett-cdn.com%2Fpresto%2F2018%2F09%2F18%2FPREN%2F47df5f97-9b6e-4df6-8e47-44789273fe91-CB_5_Guys_Bacon_Cheeseburger.jpg%3Fcrop%3D2993%2C1677%2Cx0%2Cy0%26width%3D3200%26height%3D1680%26fit%3Dbounds&f=1&nofb=1"
-            }
-            alt=""
-          />
-        </Carousel.Item>
+        <Carousel interval={4000} controls={false} indicators={false}>
+        {Students.map((student, index) => (
+            ((index%2===0) && (
+                <Carousel.Item key={index}>
+                                    <p>{index}</p>
+                    <Person 
+                        name={Students[index].name}
+                        major={Students[index].major} 
+                        primary_area={Students[index].primary_area}
+                        img={Students[index].img}
+                        email={Students[index].email}
+                        linkedin={Students[index].linkedin}
+                    />
+                    <Person 
+                        name={Students[index+1].name}
+                        major={Students[index+1].major} 
+                        primary_area={Students[index+1].primary_area}
+                        img={Students[index+1].img}
+                        email={Students[index+1].email}
+                        linkedin={Students[index+1].linkedin}
+                    />
+                </Carousel.Item>
+            ))
+        ))}
       </Carousel>
                 {/* <div>
                     <Person 
