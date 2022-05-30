8b2be2d97c910fadccb95c466d98a4a9b432e544
JCamyre <jwcamry03@gmail.com>
2022-05-29 23:23:18 -0700

    Add mainbuttons

--- displaysite/src/components/MainButtons/MainButtons.jsx
+++ displaysite/src/components/MainButtons/MainButtons.jsx
@@ -20,14 +20,7 @@ const mainButtons = {
     // gridAutoFlow: 'column'
 };
 
-const buttonStyle = {
-  display: 'flex',
-  alignItems: 'center', 
-  justifyContent: 'center', 
-  height: '100%', 
-  borderRadius: 10,
-  backgroundImage: "url(https://dinnerthendessert.com/wp-content/uploads/2015/08/Burger-B-Small-1.jpg)"
-}
+
 
 const images = [
   {
@@ -57,35 +50,19 @@ function MainButtons() {
   return (
     // Make the div stretch to fit the entire page (besides the Navbar)
     <div style={mainButtons}>
-        <Link to='news'>
-          <div style={buttonStyle}>
-              <div>
-                  yo
-              </div>
-          </div>
-        </Link>
-        <Link to='news'>
-          <div style={buttonStyle}>
-              <div>
-                  yo
-              </div>
-          </div>
-        </Link>
-        <Link to='news'>
-          <div style={buttonStyle}>
-              <div>
-                  yo
-              </div>
-          </div>
-        </Link>
-        <Link to='news'>
-          <div style={buttonStyle}>
-              <div>
-                  yo
-              </div>
-          </div>
-        </Link> 
-      </div>
+      <MainButton url='news'>
+        News
+      </MainButton>
+      <MainButton url='people'>
+        People
+      </MainButton>
+      <MainButton url='posters'>
+        Posters
+      </MainButton>
+      <MainButton url='videos'>
+        Videos
+      </MainButton>
+    </div>
   )
 }
 
