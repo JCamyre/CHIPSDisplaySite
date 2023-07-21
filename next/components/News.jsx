aab1ab4a77023a4265e4583d9661b788ec7580f6
Joseph Camyre <jwcamry03@gmail.com>
2023-07-21 18:17:10 -0400

    Fix getStaticProps() not calling in news.js. Fix function not receiving props from getStaticProps. Have temporary articles prop.

--- next/components/News.jsx
+++ next/components/News.jsx
@@ -2,15 +2,20 @@ import React, { useEffect, useState } from 'react';
 import NewsArticle from '../components/NewsArticle';
 import { Stack } from '@mui/material';
 import axios from 'axios';
-import Articles from './Articles.json';
+// import Articles from './Articles.json';
 
 function News({ articles }) {
+
+  useEffect(() => {
+    console.log(articles);
+  }, [])
 
   return (
     <Stack alignItems='center' style={{paddingTop: '20px'}}>
         <h1 style={{color: '#1f497d'}}>UCLA Engineering News</h1>
-        {Articles && Articles.map((article, key) => (
+        {articles && articles.map((article, key) => (
           <NewsArticle
+            key={key}
             id={key}
             article={article}
           />
@@ -21,18 +26,3 @@ function News({ articles }) {
 
 export default News;
 
-export const getStaticProps = async() => {
-  const articles = await axios.get('/api/get_all_articles').then(res => {
-    console.log('yo')
-    return res;
-  })
-
-  // reference .json for getStaticProps?
-
-  return {
-    props: {
-      articles,
-    },
-    revalidate: 4000,
-  }
-}
