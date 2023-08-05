e750af15f8899aabd4310042809e868e9f08b426
Joseph Camyre <jwcamry03@gmail.com>
2023-08-05 12:50:24 -0400

    Fix major issue with incorrect use of Promise, did not use the .resolve(). Remove that, simply return with .then().

--- next/components/GetAllArticles.js
+++ next/components/GetAllArticles.js
@@ -7,20 +7,21 @@ const {
 const fs = require("fs");
 
 export default function getAllArticles() {
-  return new Promise((resolve, reject) => {
-    resetArticles().then(() => {
-      scrapeAllArticles().then((articles) => {
-          // Write articles to JSON file
-          // console.log('Made it into GetAllArticles.js', JSON.stringify(articles))
-          // fs.writeFileSync(pathToJSON, JSON.stringify(articles))
-          // console.log('Wrote files to Articles.json')
-          // return JSON.stringify(articles)
-          return {}
-        })
-        .catch((err) => {
-          console.log("Error in getAllArticles(): ", err);
-          return {message: "error"}
-        });
-    });
+  // return new Promise((resolve, reject) => {
+  return resetArticles().then(() => {
+    return scrapeAllArticles().then((articles) => {
+        // Write articles to JSON file
+        // console.log('Made it into GetAllArticles.js', JSON.stringify(articles))
+        // fs.writeFileSync(pathToJSON, JSON.stringify(articles))
+        // console.log('Wrote files to Articles.json')
+        // return JSON.stringify(articles)
+        console.log('We in GetAllArticles.js', articles[0]);
+        return articles;
+      })
+      .catch((err) => {
+        console.log("Error in getAllArticles(): ", err);
+        return {message: "error"}
+      });
   });
+  // });
 }
