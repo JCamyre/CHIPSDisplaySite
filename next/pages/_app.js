b23ccf41d880c2839344e2147f4461c8f1a2f7be
JCamyre <jwcamry03@gmail.com>
2023-09-20 19:16:06 -0700

    Revert "Update poster.js to display .jpg, cutting down loading time of each poster in slideshow to 0.05s."
    This reverts commit 50fa2bf9a6c1e853e7aa3fe00bb2eb224e8da5da.

--- next/pages/_app.js
+++ next/pages/_app.js
@@ -4,7 +4,6 @@ import Navbar from "../components/Navbar";
 function MyApp({ Component, pageProps }) {
   return (
     <div style={{ height: "100vh", width: "100vw" }}>
-    {/* <meta content='True' name='HandheldFriendly' /><meta content='width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;' name='viewport' /> */}
       <Navbar />
       <Component {...pageProps} />
     </div>
